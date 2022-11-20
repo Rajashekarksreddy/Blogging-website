0ec8c285f2d288121d4586d4a7383e4b372b9eb3
Rajashekarks <rajksreddy@gmail.com>
2022-11-20 14:08:36 +0530

    deleteByid

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,7 @@ router.post('/post',authorcontroller.createAuthor )
 router.post('/createBlog',BlogController.createBlog)
 router.get('/filterblogs', BlogController.getBlogs)
 router.put('/blogs/:blogId', BlogController.updateBlogById)
+router.delete('/blogs/:blogId', BlogController.deleteBlogId)
 
 
 module.exports = router
